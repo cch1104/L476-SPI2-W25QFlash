6c9f2486f054a1f7f79e97d887ce7a9cd2c5a20d
J.C.TW <cch1104@hotmail.com>
2026-06-22 15:37:46 +0800

    feature/restructure
    1.put w25qxx function into w25qxx.c
    2.put name of function into w25qxx.h
    3.clear w25qxx function from main.c

--- Core/Inc/w25qxx.h
+++ Core/Inc/w25qxx.h
@@ -8,6 +8,34 @@
 #ifndef INC_W25QXX_H_
 #define INC_W25QXX_H_
 
+#include "main.h"
 
+#define W25Q_CS_LOW() \
+HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0, GPIO_PIN_RESET)
+
+#define W25Q_CS_HIGH() \
+HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0, GPIO_PIN_SET)
+
+
+/* Export SPI Handle */
+extern SPI_HandleTypeDef hspi2;
+
+/* Function Prototypes */
+
+void W25Q_WriteEnable(void);
+
+uint8_t W25Q_ReadStatus(void);
+
+void W25Q_WaitBusy(void);
+
+void W25Q_SectorErase(uint32_t addr);
+
+void W25Q_Read(uint32_t addr,
+               uint8_t *buf,
+               uint16_t len);
+
+void W25Q_Write(uint32_t addr,
+                uint8_t *buf,
+                uint16_t len);
 
 #endif /* INC_W25QXX_H_ */
--- Core/Src/w25qxx.c
+++ Core/Src/w25qxx.c
@@ -5,4 +5,139 @@
  *      Author: cch11
  */
 
+#include "w25qxx.h"
+
+/* Export SPI Handle */
+extern SPI_HandleTypeDef hspi2;
+
+/***********************************************
+*Write enable function
+************************************************/
+void W25Q_WriteEnable(void)
+{
+    uint8_t cmd = 0x06;
+
+    W25Q_CS_LOW();
+
+    HAL_SPI_Transmit(&hspi2,
+                     &cmd,
+                     1,
+                     HAL_MAX_DELAY);
+
+    W25Q_CS_HIGH();
+}
+
+/*************************************************
+ * *ReadStatus function
+ * ***********************************************/
+uint8_t W25Q_ReadStatus(void)
+{
+    uint8_t cmd = 0x05;
+    uint8_t status = 0;
+
+    W25Q_CS_LOW();
+
+    HAL_SPI_Transmit(&hspi2,
+                     &cmd,
+                     1,
+                     HAL_MAX_DELAY);
+
+    HAL_SPI_Receive(&hspi2,
+                    &status,
+                    1,
+                    HAL_MAX_DELAY);
+
+    W25Q_CS_HIGH();
+
+    return status;
+}
+
+/****************************************************
+ * Sector Erase function-Sector Erase-W25Q_WaitBusy function
+ * *************************************************/
+void W25Q_WaitBusy(void)
+{
+    while(W25Q_ReadStatus() & 0x01);
+}
+
+
+void W25Q_SectorErase(uint32_t addr)
+{
+    uint8_t cmd[4];
+
+    W25Q_WriteEnable();
+
+    cmd[0] = 0x20;
+    cmd[1] = (addr >> 16) & 0xFF;
+    cmd[2] = (addr >> 8) & 0xFF;
+    cmd[3] = addr & 0xFF;
+
+    W25Q_CS_LOW();
+    HAL_SPI_Transmit(&hspi2, cmd, 4, HAL_MAX_DELAY);
+    W25Q_CS_HIGH();
+
+    W25Q_WaitBusy();
+}
+
+/****************************************************
+ * Read Function
+ * *************************************************/
+void W25Q_Read(uint32_t addr,
+               uint8_t *buf,
+               uint16_t len)
+{
+    uint8_t cmd[4];
+
+    cmd[0] = 0x03;
+    cmd[1] = (addr >> 16) & 0xFF;
+    cmd[2] = (addr >> 8) & 0xFF;
+    cmd[3] = addr & 0xFF;
+
+    W25Q_CS_LOW();
+
+    HAL_SPI_Transmit(&hspi2,
+                     cmd,
+                     4,
+                     HAL_MAX_DELAY);
+
+    HAL_SPI_Receive(&hspi2,
+                    buf,
+                    len,
+                    HAL_MAX_DELAY);
+
+    W25Q_CS_HIGH();
+}
+
+/****************************************************
+*Write Function
+****************************************************/
+void W25Q_Write(uint32_t addr,
+                uint8_t *buf,
+                uint16_t len)
+{
+    uint8_t cmd[4];
+
+    W25Q_WriteEnable();
+
+    cmd[0] = 0x02;
+    cmd[1] = (addr >> 16) & 0xFF;
+    cmd[2] = (addr >> 8) & 0xFF;
+    cmd[3] = addr & 0xFF;
+
+    W25Q_CS_LOW();
+
+    HAL_SPI_Transmit(&hspi2,
+                     cmd,
+                     4,
+                     HAL_MAX_DELAY);
+
+    HAL_SPI_Transmit(&hspi2,
+                     buf,
+                     len,
+                     HAL_MAX_DELAY);
+
+    W25Q_CS_HIGH();
+
+    W25Q_WaitBusy();
+}
 
